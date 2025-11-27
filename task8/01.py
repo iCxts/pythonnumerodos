f4267d15b8e094ef49c82e14b0c416fc37fd96e2
iCxt <101859095+iCxts@users.noreply.github.com>
2025-11-27 14:04:52 +0700

    Update 01.py

--- task8/01.py
+++ task8/01.py
@@ -1,15 +1,4 @@
 '''
-<<<<<<< HEAD
-1. Pretend there is no way to get i-th element of the list. Also we don't know how to use `for` loop. Given that, print the 3rd element from `['a', 'b', 'c']` list
-'''
-sample = ['a', 'b', 'c']
-
-it = iter(sample)
-it.__next__()
-it.__next__()
-third_element = it.__next__()
-print(third_element)
-=======
 Write a function cut_suffix which takes a string and a suffix. A function should return this string without the given suffix.
 
 cut_suffix("foobar", "bar")
@@ -20,5 +9,3 @@ cut_suffix("foobar", "boo")
 '''
 def cut_suffix(word: str, suffix: str) -> str:
     return word.removesuffix(suffix)
-
->>>>>>> bd86a8bab30c1319eb447da262f2835cd0021dfc
